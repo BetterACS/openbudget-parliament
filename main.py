5bc441ebdbe3ccdbf95d80a54f134e175ad2ca8e
monchinawat <monshinawatra@gmail.com>
2024-08-06 23:28:22 +0700

    cors fix

--- main.py
+++ main.py
@@ -15,7 +15,15 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-app.add_middleware(CORSMiddleware, allow_origins=["*"])
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 new_prompt = PromptTemplate(
     """\
