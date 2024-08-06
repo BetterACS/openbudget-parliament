0b6b562dbc80e9af59c455138b809c92b4873f50
monchinawat <monshinawatra@gmail.com>
2024-08-06 22:31:28 +0700

    Add jungwat

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ dotenv.load_dotenv()
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(CORSMiddleware, allow_origins=["*"])
 
 new_prompt = PromptTemplate(
     """\
@@ -28,12 +29,13 @@ column references
 จงทำการค้นหาข้อมูลเกี่ยวกับ 
 {instruction_str}
 
+หากเกี่ยวข้องกับจังหวัดกรุณาเติมคำว่า 'จังหวัด' ตามด้วยชื่อจังหวัดในการค้นหา
 คำถาม: {query_str}
 
 คำตอบ: """
 )
 
-instruction_str = """\
+instruction_str = """\    
 1. Convert the query to executable Python code using Pandas.
 2. The final line of code should be a Python expression that can be called with the `eval()` function.
 3. The code should represent a solution to the query.
@@ -41,6 +43,9 @@ instruction_str = """\
 5. Do not quote the expression.
 """
 
+with open("jungwat.txt", "r", encoding="utf-8") as f:
+    jungwats = f.readlines()
+jungwats = [j.strip() for j in jungwats]
 
 df = pd.read_csv("[67] Thailand's Budget - ขาวคาดแดง RELEASE_2023-12-30.csv")
 df["AMOUNT"] = df["AMOUNT"].str.replace(",", "").astype(float)
@@ -69,8 +74,24 @@ def bento(query: str):
     response = query_pipeline(query)
     results_df_query = eval(response.metadata["pandas_instruction_str"])
 
+    if query.replace("จังหวัด", "").replace("เมือง", "") == "พัทยา":
+        results_df_query = df[df["BUDGETARY_UNIT"].str.contains("พัทยา")]
+
+    # results_df_query = results_df_query[results_df_query["BUDGETARY_UNIT"].str.contains(query.replace("จังหวัด", ""))]
+    # if query.replace("จังหวัด", "") in ["กรุงเทพ", "กรุงเทพมหานคร", "กทม", "เมืองพัทยา", "พัทยา"]:
+    #     results_df_query = results_df_query[results_df_query["MINISTRY"] == "องค์กรปกครองส่วนท้องถิ่น"]
+    # el
+    if query.replace("จังหวัด", "") in jungwats:
+        results_df_query = results_df_query[
+            (results_df_query["MINISTRY"] == "จังหวัดและกลุ่มจังหวัด")
+            | (results_df_query["MINISTRY"] == "องค์กรปกครองส่วนท้องถิ่น")
+        ]
+        # return str(results_df_query)
+
     return {
-        "categories": results_df_query.groupby("CATEGORY_LV1")["AMOUNT"].sum().to_dict(),
+        "categories": [(k, v) for k, v in results_df_query.groupby("CATEGORY_LV1")["AMOUNT"].sum().to_dict().items()],
+        "strategies": [(k, v) for k, v in results_df_query.groupby("STRATEGY")["AMOUNT"].count().to_dict().items()],
+        "ministry": [(k, v) for k, v in results_df_query.groupby("MINISTRY")["AMOUNT"].count().to_dict().items()],
         "total": results_df_query["AMOUNT"].sum(),
         "total_projects": len(results_df_query),
     }
