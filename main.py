af88c0fae6c35a40374b2246ce73ec4a5ee394d3
monchinawat <monshinawatra@gmail.com>
2024-08-06 23:40:51 +0700

    Fix cors 2

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-origins = ["*"]
+origins = [
+    "http://localhost:3000",
+]
 
 app.add_middleware(
     CORSMiddleware,
